60db17bf975c2d88491c73ba12ae21411ac860ee
David Ralph <David.M.L.Ralph@gmail.com>
2019-10-20 08:35:58 +0100

    Added magnet repulsion

--- src/utils/attractors.js
+++ src/utils/attractors.js
@@ -5,7 +5,7 @@ import { goName, mag } from './general'
 
 export function earthAttractor(earth, other) {
     const distance = Math.abs(mag(other.position, earth.position))
-    const d2 = distance^2;
+    const d2 = distance^2
     return  {
         x: Math.sign(earth.position.x - other.position.x) * 0.001/d2,
         y: Math.sign(earth.position.y - other.position.y) * 0.001/d2,
@@ -18,14 +18,28 @@ export function junkerAttractor(junker, other) {
     }
 
     const distance = Math.abs(mag(other.position, junker.position))
+    const d2 = distance^2
 
+    // Out of range
     if (distance > 100) {
         return 0
     }
 
-    return {
-        x: Math.sign(junker.position.x - other.position.x) * 0.00001,
-        y: Math.sign(junker.position.y - other.position.y) * 0.00001,
+
+    // Push away
+    else if( distance < junker.shape.radius + 10){
+        return {
+            x: -Math.sign(junker.position.x - other.position.x) * 0.000001,
+            y: -Math.sign(junker.position.y - other.position.y) * 0.000001,
+        }
+    }
+
+    // Pull towards
+    else{
+        return {
+            x: Math.sign(junker.position.x - other.position.x) * 0.0005/d2,
+            y: Math.sign(junker.position.y - other.position.y) * 0.0005/d2,
+        }
     }
 }
 
